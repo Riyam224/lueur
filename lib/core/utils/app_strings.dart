54ed25af66708cb3f295ade60e892d0aa659b301
Riyam <cs50riyam211@gmail.com>
2026-06-26 00:31:07 +0300

    add comments to app strings

--- lib/core/utils/app_strings.dart
+++ lib/core/utils/app_strings.dart
@@ -1,5 +1,7 @@
 /// App-wide string constants shared across features
 class AppStrings {
+  // splash
   static const String appName = 'Lueur';
   static const String appTagline = 'a little light for you';
+  // onboarding
 }
