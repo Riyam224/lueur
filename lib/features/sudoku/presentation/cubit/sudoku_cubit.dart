803fe293b52ac938e200c4de30c66445b59f164d
Riyam <cs50riyam211@gmail.com>
2026-09-01 18:12:38 +0300

    fix issues with onboarding when register with new account

--- lib/features/sudoku/presentation/cubit/sudoku_cubit.dart
+++ lib/features/sudoku/presentation/cubit/sudoku_cubit.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:logger/logger.dart';
 import 'package:lueur/core/journal/journal_refresh_signal.dart';
